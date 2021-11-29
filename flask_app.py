0799c58e76b30a655b344c4d30af46ddec7fe8e4
maxposchmann <maxposchmann@gmail.com>
2021-11-28 22:31:41 -0500

    Copy files from Dave's repo

---
+++ flask_app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from config import Config
+
+server = Flask(__name__)
+server.config.from_object(Config)
+
+from flask import Blueprint, render_template, redirect, request, url_for
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap(server)
+server_bp = Blueprint('main', __name__)
+
+from forms import selectTeam
+import pandas as pd
+from SelfConsistentCFBRanker.functions import general as gen
+
+db = 'SelfConsistentCFBRanker/ncaafb.p'
+d= pd.read_pickle(db)
+
+@server_bp.route('/',methods=['GET', 'POST'])
+def index():
+    table = d['analysis']['teamRankings'].copy()
+    table = gen.formatDf(table)
+
+    week = d['analysis']['week']
+    return render_template('index.html', title = 'Index', table=table, week=week)
+
+@server_bp.route('/team_stats',methods=['GET', 'POST'])
+def team_stats():
+    form = selectTeam()
+    team = form.myField.data
+    data = d['analysis']['byTeam'][team]
+
+    if request.method == "POST":
+        team = form.myField.data
+        data = d['analysis']['byTeam'][team]
+
+    return render_template('teamStats.html', data=data,  form = form)
+
+server.register_blueprint(server_bp)
