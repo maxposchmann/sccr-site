fa6ac6d28838e28753244ab60591025547f40f17
maxposchmann <maxposchmann@gmail.com>
2021-11-30 10:04:56 -0500

    add team data

--- flask_app.py
+++ flask_app.py
@@ -23,18 +23,24 @@ def index():
     table = gen.formatDf(table)
 
     week = d['analysis']['week']
-    return render_template('index.html', title = 'Index', table=table, week=week)
+    return render_template('index.html', title='Index', table=table, week=week)
 
 @server_bp.route('/team_stats',methods=['GET', 'POST'])
 def team_stats():
     form = selectTeam()
     team = form.myField.data
     data = d['analysis']['byTeam'][team]
+    stats = d['analysis']['byTeam'][team]['stats'].copy()
+    stats = gen.formatDf(stats)
+    results = d['analysis']['byTeam'][team]['results'].copy()
+    results = gen.formatDf(results)
+    remaining = d['analysis']['byTeam'][team]['remaining'].copy()
+    remaining = gen.formatDf(remaining)
 
     if request.method == "POST":
         team = form.myField.data
         data = d['analysis']['byTeam'][team]
 
-    return render_template('teamStats.html', data=data,  form = form)
+    return render_template('teamStats.html', data=data, form=form, stats=stats, results=results, remaining=remaining)
 
 server.register_blueprint(server_bp)
