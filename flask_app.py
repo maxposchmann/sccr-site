3a99404e4fe13a8db590311f51ae30559bdafeb3
maxposchmann <maxposchmann@gmail.com>
2021-12-03 13:46:39 -0500

    use url parameters to get team from link

--- flask_app.py
+++ flask_app.py
@@ -28,20 +28,35 @@ def index():
 @server_bp.route('/team_stats',methods=['GET', 'POST'])
 def team_stats():
     form = selectTeam()
-    team = form.myField.data
-    data = d['analysis']['byTeam'][team]
-    stats = d['analysis']['byTeam'][team]['stats'].copy()
-    stats = gen.formatDf(stats)
-    results = d['analysis']['byTeam'][team]['results'].copy()
-    results = gen.formatDf(results)
-    remaining = d['analysis']['byTeam'][team]['remaining'].copy()
-    remaining = gen.formatDf(remaining)
+    form_team = form.team.data
+    link_team = request.args.get('team')
 
-    if request.method == "POST":
-        team = form.myField.data
-        data = d['analysis']['byTeam'][team]
+    if form_team == None and link_team == None:
+        return render_template('selectTeam.html', form=form)
+    elif form_team == link_team or form_team == None:
+        if form.team.data == None:
+            form.team.data = link_team
+        team = form.team.data
+        try:
+            data = d['analysis']['byTeam'][team]
+            stats = d['analysis']['byTeam'][team]['stats'].copy()
+            stats = gen.formatDf(stats)
+            results = d['analysis']['byTeam'][team]['results'].copy()
+            results = gen.formatDf(results)
+            remaining = d['analysis']['byTeam'][team]['remaining'].copy()
+            remaining = gen.formatDf(remaining)
 
-    return render_template('teamStats.html', data=data, form=form, stats=stats, results=results, remaining=remaining)
+            if request.method == "POST":
+                team = form.team.data
+                data = d['analysis']['byTeam'][team]
+            return render_template('teamStats.html', data=data, form=form, stats=stats, results=results, remaining=remaining)
+        except KeyError:
+            return redirect(f'/team_stats')
+    else:
+        if form.team.data == None:
+            form.team.data = link_team
+        team = form.team.data
+        return redirect(f'/team_stats?team={team}')
 
 @server_bp.route('/explanation',methods=['GET', 'POST'])
 def explanation():
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SelectField,SubmitField,TextAreaField
-from wtforms.validators import DataRequired,Length, InputRequired
+from wtforms.validators import DataRequired, Length, InputRequired
 from datetime import datetime
 
 import pandas as pd
@@ -10,4 +10,4 @@ d = pd.read_pickle(db)
 teams = d['data']['teams']
 
 class selectTeam(FlaskForm):
-    myField = SelectField('Team', choices=teams,default =teams[0], validators=[DataRequired()])
+    team = SelectField('Team', choices = teams, validators = [DataRequired()])
